362744a9431742428da7571b7cb15245d98a3b15
batelpirov1@gmail.com <batelpirov1@gmail.com>
2023-05-13 22:17:04 +0300

    Initial commit

---
+++ src/main/java/com/Millennium/InMemoryCache/InMemoryCacheApplication.java
@@ -0,0 +1,14 @@
+package com.Millennium.InMemoryCache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemoryCacheApplication.class, args);
+		System.out.println("start");
+	}
+
+}
