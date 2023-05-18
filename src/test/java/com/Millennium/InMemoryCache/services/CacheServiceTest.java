48ab4922e0ff7caccbe8772b181ea2e2c46cf247
batelpirov1@gmail.com <batelpirov1@gmail.com>
2023-05-19 00:20:00 +0300

    remove test

--- src/test/java/com/Millennium/InMemoryCache/services/CacheServiceTest.java
+++ src/test/java/com/Millennium/InMemoryCache/services/CacheServiceTest.java
@@ -5,12 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+
 
 @SpringBootTest
 class CacheServiceTest {
@@ -40,31 +36,5 @@ class CacheServiceTest {
 		Object retrievedValue = cacheService.get(key);
 
 		assertEquals(value2, retrievedValue);
-	}
-
-	@Test
-	public void testConcurrentRequests() throws InterruptedException {
-		CacheService<String, Integer> cacheService = new CacheService<>();
-		int numRequests = 1000;
-
-		ExecutorService executorService = Executors.newFixedThreadPool(numRequests);
-
-		for (int i = 0; i < numRequests; i++) {
-			final int requestNumber = i;
-			executorService.submit(() -> {
-				String key = "Key" + requestNumber;
-				Integer value = requestNumber;
-				cacheService.put(key, value);
-				Integer retrievedValue = cacheService.get(key);
-				assertEquals(value, retrievedValue);
-			});
-		}
-
-		executorService.shutdown();
-		executorService.awaitTermination(1, TimeUnit.MINUTES);
-
-		// Verify the expected behavior
-		verify(cacheService, times(numRequests)).put(anyString(), any(Integer.class));
-		verify(cacheService, times(numRequests)).get(anyString());
 	}
 }
